a897efd9f9676fff18c5c2c2491c6d4f70852dc8
mike <scherbakov.mike@gmal.com>
2022-03-31 20:56:58 +0300

    initial

--- app/src/main/java/com/example/tetris/models/Block.java
+++ app/src/main/java/com/example/tetris/models/Block.java
@@ -3,6 +3,10 @@ package com.example.tetris.models;
 import android.graphics.Color;
 import android.graphics.Point;
 
+import androidx.annotation.NonNull;
+
+import com.example.tetris.constants.FieldConstants;
+
 import java.util.Random;
 
 public class Block {
@@ -15,15 +19,15 @@ public class Block {
         this.frameNumber = 0;
         this.shapeIndex = shapeIndex;
         this.color = blockColor;
-        this.position = new Point(AppModel.FieldConstants.COLUMN_COUNT.getValue()/2, 0);
+        this.position = new Point(FieldConstants.COLUMN_COUNT.getValue() / 2, 0);
     }
 
     public enum BlockColor {
-        PINK(Color.rgb(255,105,180), (byte) 2),
-        GREEN(Color.rgb(0,128,0), (byte) 3),
-        ORANGE(Color.rgb(255,140,0), (byte) 4),
-        YELLOW(Color.rgb(255,255,0), (byte) 5),
-        CYAN(Color.rgb(0,255,255), (byte) 6);
+        PINK(Color.rgb(255, 105, 180), (byte) 2),
+        GREEN(Color.rgb(0, 128, 0), (byte) 3),
+        ORANGE(Color.rgb(255, 140, 0), (byte) 4),
+        YELLOW(Color.rgb(255, 255, 0), (byte) 5),
+        CYAN(Color.rgb(0, 255, 255), (byte) 6);
 
         BlockColor(int rgbValue, byte value) {
             this.rgbValue = rgbValue;
@@ -42,6 +46,40 @@ public class Block {
         Block block = new Block(shapeIndex, blockColor);
         block.position.x = block.position.x - Shape.values()[shapeIndex].getStartPosition();
         return block;
+    }
+
+    public static int getColor(byte value) {
+        for (BlockColor colour : BlockColor.values()) {
+            if (value == colour.byteValue)
+                return colour.rgbValue;
+        }
+        return -1;
+    }
+
+    public final void setState(int frame, Point position) {
+        this.frameNumber = frame;
+        this.position = position;
+    }
+
+    @NonNull
+    public final byte[][] getShape(int frameNumber) {
+        return Shape.values()[shapeIndex].getFrame(frameNumber).as2dByteArray();
+    }
+
+    public Point getPosition() {
+        return this.position;
+    }
+
+    public final int getFrameCount() {
+        return Shape.values()[shapeIndex].getFrameCount();
+    }
+
+    public int getColor() {
+        return color.rgbValue;
+    }
+
+    public byte getStaticValue() {
+        return color.byteValue;
     }
 
 }
